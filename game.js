312c4f418685fb413176146c099b3d20b11bfbd0
Carlos Garcia <carlosgarciahernandez1999@gmail.com>
2023-03-28 12:57:59 +0100

    movement is generated and the astronaut turns to the corresponding side

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
 import Platform from './plataform.js'
 import Astronaut from './astronaut.js'
+import game from './index.js'
 
 function Game() {
     this.canvas = document.querySelector('#canvas')
     this.astronaut
     this.platforms = []
     this.timerId
+    this.isMoving =false
+    this.count = 0
 }
 Game.prototype.createPlatforms = function (platformCount){
     for (let i = 0; i < platformCount; i++) {
@@ -32,37 +35,62 @@ Game.prototype.astronautInitial = function () {
 }
 Game.prototype.moveAstronaut = function (){
     document.addEventListener('keydown', control)
+    let astronautVisual = document.querySelector('.astronaut')
     let self = this
     function control(e) {
         if (e.key === 'ArrowLeft') {
+            astronautVisual.classList.add('astronautLet')
+            astronautVisual.classList.remove('astronautRigth')
             self.astronaut.left()
         } else if (e.key === 'ArrowRight') {
+            astronautVisual.classList.add('astronautRigth')
+            astronautVisual.classList.remove('astronautLet')
             self.astronaut.rigth()
         }
     }
 }
 Game.prototype.checkMove = function (steps){
-   
+   console.log("func")
+    this.isMoving = false
+
     for(let i = 0; i< steps; i++){
-        this.remove()
         
-        for (let i = 0; i < this.platforms.length; i++) {
-            let distance = this.platforms[i].bottom
-            distance -= 120
-            this.platforms[i].bottom -= 120
-            this.platforms[i].visual.style.bottom = distance + 'px'
-        }
-        this.updatePlataforms()
-    }    
+        if (!this.isMoving) {
+            game.count = 0
 
-
-    
+            this.timerId=  setInterval (this.move, 70 )
+            this.isMoving = true
+        }    
+    }   
        
 }
+Game.prototype.move = function(){
 
+        for (let i = 0; i < game.platforms.length; i++) {
+            let distance = game.platforms[i].bottom
+            distance -= 12
+            game.platforms[i].bottom -= 12
+            game.platforms[i].visual.style.bottom = distance + 'px'
+            game.count = game.count +1
+            console.log(game.count)
+            
+        }
+    if (game.count == 60){
+            game.remove()
+            game.updatePlataforms()
+            clearInterval(game.timerId)
+        }
+        
+
+    
+}
 Game.prototype.remove = function (){
     this.canvas.removeChild(this.platforms[0].visual)
     this.platforms.shift()
 }
-
+ /*
+    distance -= 120
+    this.platforms[i].bottom -= 120
+    this.platforms[i].visual.style.bottom = distance + 'px'
+ */
 export default Game
